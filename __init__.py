c78b5c02f8ac642f837d088d7fa44ea5a62ae17b
multipapl <mykytapapl@gmail.com>
2025-08-25 23:23:00 +0300

    Bump version to 1.0.93 and update addon metadata in __init__.py for clarity and completeness.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+bl_info = {
+    "name": "Papl Tools",
+    "author": "Papl",
+    "version": (1, 0, 93),  # Поточна версія
+    "blender": (4, 2, 0),  # Мінімальна версія Blender для роботи
+    "location": "View3D > Sidebar > Papl_Tools", # Де шукати аддон в інтерфейсі
+    "description": "Короткий опис того, що робить аддон",
+    "warning": "",
+    "doc_url": "", # Посилання на документацію, якщо є
+    "category": "Object", # Категорія в списку аддонів
+}
+
 import bpy
 from . import panel, operators
 
